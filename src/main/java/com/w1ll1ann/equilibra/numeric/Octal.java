f396ba9ff7349e26f899311101e950a6e2885c8d
w1ll1ann <willearth255@gmail.com>
2024-12-14 19:46:01 -0300

    Add Numeric classes and define its tests

---
+++ src/main/java/com/w1ll1ann/equilibra/numeric/Octal.java
@@ -0,0 +1,20 @@
+package com.w1ll1ann.equilibra.numeric;
+
+public class Octal {
+
+    static void toDecimal(String inputValue) {
+
+    }
+
+    static void toHexadecimal(String inputValue) {
+
+    }
+
+    static void toBinary(String inputValue) {
+
+    }
+
+    public static String convert(String inputValue, String conversionUnit) {
+        return "";
+    }
+}
